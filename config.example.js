89d44a732254d9483e13f48106721eb0350430b0
Bram <84628826+sys-256@users.noreply.github.com>
2022-03-07 18:22:34 +0100

    move to import statements +  improve code  + update depends

---
+++ config.example.js
@@ -0,0 +1,6 @@
+export default {
+    DISCORD_API_KEY: ``,
+    TENOR_API_KEY: ``,
+    ACTIVITY_TYPE: ``,
+    ACTIVITY: ``
+};
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 // Import config file
-const config = require("./config.json");
+import config from "./config.js";
 
 // Required packages
-const Discord = require("discord.js");
-const client = new Discord.Client();
-const fetch = require("node-fetch");
-const os = require("os");
-const os_utils = require("node-os-utils");
+import Discord from "discord.js";
+const client = new Discord.Client({
+    intents: [`GUILDS`, `GUILD_MESSAGES`,],
+});
+import fetch from "node-fetch"
+import os from "os";
+import os_utils from "node-os-utils";
 
 // Format seconds function
 function format(seconds) {
@@ -18,15 +20,12 @@ function format(seconds) {
 
 // Creates the get_gif_url function
 async function get_gif_url(query) {
-    try {
-        // Fetches the gif url
-        const gif_response = await fetch(`https://g.tenor.com/v1/search?q=${query}&key=${config.TENOR_API_KEY}&limit=1&media_filter=minimal`);
-        // Converts it to json
-        const gif_data = await gif_response.json();
-        // Returns the url
-        return gif_data.results[0].media[0].gif.url;
-    }
-    catch { }
+    // Fetches the gif url
+    const gif_response = await fetch(`https://g.tenor.com/v1/search?q=${query}&key=${config.TENOR_API_KEY}&limit=1&media_filter=minimal`);
+    // Converts it to json
+    const gif_data = await gif_response.json();
+    // Returns the url
+    return gif_data.results[0].media[0].gif.url;
 }
 
 // Sets the user activity
@@ -38,33 +37,29 @@ client.on("ready", () => {
 });
 
 // Executes when someone sends a message
-client.on("message", async (message) => {
-    // Makes sure the user isn't a bot
-    if (message.author.bot) return;
-    // Makes sure the message starts with the prefix
-    if (!message.content.toLowerCase().startsWith("gif ")) return;
+client.on("messageCreate", async (message) => {
+    // Make sure the user isn't a bot and that the message that's with the prefix
+    if (message.author.bot || !message.content.toLowerCase().startsWith("gif ")) return;
 
-    // Gets the command and args from the sended message
-    const commandBody = message.content.slice(4);
-    const args = commandBody.split(" ");
-    const command = args.shift().toLowerCase();
+    // Gets the command and args from the sent message
+    const command = message.content.slice(4).split(" ")[0].toLowerCase();
 
     // If the text is "help", it will show the help message
     if (command === "help") {
         const embed = new Discord.MessageEmbed()
-        .setTitle(`Hello!`)
-        .setDescription(`
+            .setTitle(`Hello!`)
+            .setDescription(`
 Thanks for using this bot, to use it, just type 'gif <gif you want to search>'. Easy as that!
 
 PS: [You can upvote me on Top.GG!](https://top.gg/bot/867011965988503562)`)
 
-        message.reply(embed);
+        message.reply({ embeds: [embed] });
     }
     // If the text is "stats", it will show the stats message
     else if (command === "stats") {
         const embed = new Discord.MessageEmbed()
-        .setTitle(`Stats:`)
-        .setDescription(`
+            .setTitle(`Stats:`)
+            .setDescription(`
 Version: 1.2
 Invite: [click here](https://discord.com/api/oauth2/authorize?client_id=867011965988503562&permissions=2048&scope=bot)
 
@@ -76,12 +71,13 @@ RAM usage: ${Math.round((os.totalmem() - os.freemem()) / 1000000)}MB
 
 Process uptime: ${format(process.uptime())}
 Server uptime: ${format(os.uptime())}
-        `)
-        message.reply(embed)
+        `);
+
+        message.reply({ embeds: [embed] });
     }
-    // If the command is not "help", it will fetch the gif and send it
+    // Fetch the gif and send it
     else {
-        message.channel.send(await get_gif_url(commandBody));
+        message.channel.send(await get_gif_url(message.content.slice(4).split(" ")[0]));
     }
 });
 
